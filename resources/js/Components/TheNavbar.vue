43f6de7357bb5bf0025f011dc5f13ae5ca479e5b
Sujith K <sujith.k@cubettech.com>
2023-02-17 12:00:53 +0530

    initial commit

---
+++ resources/js/Components/TheNavbar.vue
@@ -0,0 +1,72 @@
+<script setup>
+import TokenService from "../services/TokenService";
+import API from "../utils/API";
+
+
+const logout = async () => {
+  console.log("logout now");
+  try {
+    // const response = await axios.get("http://localhost:8000/api/logout");
+    const response = await API.get("logout");
+    console.log(response.data);
+    // localStorage.clear();
+
+    TokenService.clearToken()
+
+    location.reload();
+  } catch (e) {
+    console.error(e);
+  }
+};
+</script>
+
+<template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <nav class="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
+                <div class="container">
+                    <a class="navbar-brand mx-auto d-lg-none" href="index.html">
+                        Medic Care
+                        <strong class="d-block">Health Specialist</strong>
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#hero">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#about">About</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#timeline">Timeline</a>
+                            </li>
+
+                            <a class="navbar-brand d-none d-lg-block" href="index.html">
+                                Medic Care
+                                <strong class="d-block">Health Specialist</strong>
+                            </a>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#reviews">Testimonials</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#booking">Booking</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#contact">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+</template>
+
---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import "./bootstrap";
+
+import { createApp } from "vue";
+import App from "./Pages/app.vue";
+import router from "./router";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+const app = createApp(App)
+app.use(ElementPlus, {zIndex: 3000 })
+app.use(router).mount("#app");
